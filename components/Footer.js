e2db258fbf5a026400934c575ab50a290c7eace6
Somojit Arambam <somoarambam123@gmail.com>
2022-11-17 15:46:37 +0530

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="grid grid-cols-1  md:grid-cols-4 gap-y-10 px-32 bg-gray-100  text-gray-600 py-14">
+      <div className="  space-y-4 text-sm text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <h5 className="font-bold">How Airbnb works</h5>
+        <h5 className="font-bold">Newsrooms</h5>
+        <h5 className="font-bold">Investors</h5>
+        <h5 className="font-bold">Airbnb Luxe</h5>
+      </div>
+      <div className="space-y-4 text-sm text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <h5 className="font-bold">Accessibility</h5>
+        <h5 className="font-bold">This is not a real site</h5>
+        <h5 className="font-bold">It is a pretty awesome clone</h5>
+        <h5 className="font-bold">Referrels accepted</h5>
+      </div>
+      <div className="space-y-4 text-sm text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <h5 className="font-bold">How Airbnb works</h5>
+        <h5 className="font-bold">Newsrooms</h5>
+        <h5 className="font-bold">Investors</h5>
+        <h5 className="font-bold">Airbnb Luxe</h5>
+      </div>
+      <div className="space-y-4 text-sm text-gray-800 ">
+        <h5 className="font-bold">SUPPORT</h5>
+        <h5 className="font-bold">Help Centre</h5>
+        <h5 className="font-bold">Trust & Safety</h5>
+        <h5 className="font-bold">Investors</h5>
+        <h5 className="font-bold">Airbnb Luxe</h5>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ components/Header.js
@@ -0,0 +1,57 @@
+import Image from "next/image";
+import {
+  GlobeAltIcon,
+  Bars3Icon,
+  UserCircleIcon,
+  UserIcon,
+} from "@heroicons/react/24/solid";
+
+const Header = () => {
+  return (
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
+      {/* left */}
+      <div className="relative flex items-center h-10 cursor-pointer my-auto">
+        <Image
+          alt="logo"
+          src="https://links.papareact.com/qd3"
+          layout="fill"
+          objectFit="contain"
+          objectPosition="left"
+        />
+      </div>
+      {/* middle */}
+      <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm ">
+        <input
+          type="text"
+          className="pl-5 text-gray-600 placeholder-gray-400 text-sm bg-transparent outline-none flex-grow"
+          placeholder="Start your search"
+        />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="hidden md:inline-flex h-8 md:mx-2 bg-red-400 text-white rounded-full p-2 cursor-pointer"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+          />
+        </svg>
+      </div>
+      {/* right */}
+      <div className="flex items-center justify-end space-x-4 text-gray-500">
+        <p className="hidden md:inline-flex cursor-pointer">Become a host</p>
+        <GlobeAltIcon className="h-6 cursor-pointer" />
+        <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
+          <Bars3Icon className="h-6" />
+          <UserCircleIcon className="h-6" />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
